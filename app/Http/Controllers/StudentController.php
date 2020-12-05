a8903fc4346a14810c0478c7bdb516debd1e2331
unknown <700mirash@gmail.com>
2020-12-05 19:09:59 +0600

    Pagination increased to 50

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -260,7 +260,7 @@ $data = $this->paginate($score);
         return view('student_leaderboard', compact('data'));
     }
 
-    public function paginate($items, $perPage = 5, $page = null, $options = [])
+    public function paginate($items, $perPage = 50, $page = null, $options = [])
     {
         $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
         $items = $items instanceof Collection ? $items : Collection::make($items);
